9ef615cf3bf0b0f91562bfe798ce5b7c4a5dd52b
Stefan <stefan.meijer3@student.nhlstenden.com>
2022-05-16 15:26:13 +0200

    update db structure

--- projectmanagementapp.sql
+++ projectmanagementapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 16 mei 2022 om 15:16
+-- Gegenereerd op: 16 mei 2022 om 15:25
 -- Serverversie: 10.4.20-MariaDB
 -- PHP-versie: 8.0.9
 
@@ -58,7 +58,7 @@ CREATE TABLE `projectmember` (
   `user_id` int(8) NOT NULL,
   `project_id` int(8) NOT NULL,
   `role_id` int(8) NOT NULL,
-  `reward_points` int(8) NOT NULL
+  `reward_points` int(8) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -71,6 +71,18 @@ CREATE TABLE `role` (
   `id` int(8) NOT NULL,
   `name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `role`
+--
+
+INSERT INTO `role` (`id`, `name`) VALUES
+(1, 'voorzitter'),
+(2, 'vicevoorzitter'),
+(3, 'planner'),
+(4, 'kwaliteitscontroleur'),
+(5, 'notulist'),
+(6, 'projectlid');
 
 -- --------------------------------------------------------
 
@@ -151,8 +163,7 @@ ALTER TABLE `project`
 -- Indexen voor tabel `projectmember`
 --
 ALTER TABLE `projectmember`
-  ADD PRIMARY KEY (`user_id`,`project_id`),
-  ADD KEY `roleid` (`role_id`);
+  ADD PRIMARY KEY (`user_id`,`project_id`);
 
 --
 -- Indexen voor tabel `role`
@@ -170,9 +181,7 @@ ALTER TABLE `schedule_line`
 -- Indexen voor tabel `timesheet`
 --
 ALTER TABLE `timesheet`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `project_id` (`project_id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexen voor tabel `user`
@@ -200,7 +209,7 @@ ALTER TABLE `project`
 -- AUTO_INCREMENT voor een tabel `role`
 --
 ALTER TABLE `role`
-  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT voor een tabel `schedule_line`
