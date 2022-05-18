ead4201c761eda9ab646b16ead16978aa3ddbd4d
BartHerms <91067528+BartHerms@users.noreply.github.com>
2022-05-18 08:35:40 +0200

    Eerste setup voor planning
    Eerste setup voor planning

---
+++ handlers/planning/planningHandler.php
@@ -0,0 +1,115 @@
+<?php
+require_once('../../functions/database/dbconnect.php');
+require_once('../../functions/anti-cors/anticors.php');
+
+/**
+ * Getting posted data from the app
+ */
+$json = file_get_contents('php://input');
+$arr = json_decode($json, TRUE); // returns array("username" => "stefan") etc.
+
+if (isset($arr)) {
+   //  $planning = htmlentities($arr['planning']);
+    $id = htmlentities($arr['id']);
+    $week = htmlentities($arr['week']);
+    $activiteit = htmlentities($arr['activiteit']);
+    $project_id = htmlentities($arr['project_id']);
+
+    //Validate fields
+    if ($error = validateFields($planning, $id, $week, $activiteit, $project_id)) {
+        echo json_encode($error);
+    } else {
+        //Verify email does not exist
+        if ($error = checkEmailInDataBase($conn, $planning)) {
+            echo json_encode($error);
+        } else {
+           // $planning = "";
+            $id = "";
+            $week = "";
+            $activiteit = "";
+            $project_id = "";
+    
+            $query = "INSERT INTO shedule_line (id, week, activiteit, project_id) VALUES (?,?,?,?)";
+    
+            //Sending data to the database
+            $stmt = mysqli_prepare($conn, $query);
+            mysqli_stmt_bind_param($stmt, "sisi", $planning, $week, $activiteit, $project_id);
+            mysqli_stmt_execute($stmt);
+            
+            //All value's that will be send back to the application
+            $userValues[0]['id'] = mysqli_insert_id($conn);
+            $userValues[0]['name'] = $name;
+            $userValues[0]['email'] = $email;
+            $userValues[0]['dateOfBirth'] = $dateOfBirth;
+            // $userValues['password'] = $password; //Probaly won't send...
+    
+            //Close the statement and connection
+            mysqli_stmt_close($stmt);
+            mysqli_close($conn);
+    
+            //Send back response (JSON)
+            echo json_encode($userValues);
+        }
+    }
+} else {
+    echo json_encode('No data send');
+}
+
+/**
+ * Function to validate fields
+ */
+function validateFields ($name, $email, $dateOfBirth, $password, $confirmPassword) {
+    $error = array();
+
+    if (!isset($name) || !filter_var($name, FILTER_SANITIZE_SPECIAL_CHARS) || !preg_match('/^[A-Za-z][A-Za-z0-9]{0,49}$/', $name)) {
+        $error[] = 'name_incorrect';
+    }
+    if (!isset($email) || !filter_var($email, FILTER_SANITIZE_EMAIL)) {
+        $error[] = 'email_incorrect';
+    }
+    //Hier regex voor datum <----------------------------------!-!------------------------------------->
+    if (!isset($dateOfBirth) || !filter_var($dateOfBirth, FILTER_SANITIZE_SPECIAL_CHARS) || !preg_match('/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/', $dateOfBirth)) {
+        $error[] = 'dateOfBirth_incorrect';
+    }
+    if (!isset($password) || !filter_var($password, FILTER_SANITIZE_SPECIAL_CHARS) || !preg_match('/^[A-Za-z][A-Za-z0-9]{0,254}$/', $password)) {
+        $error[] = 'password_incorrect';
+    }
+    if (!isset($confirmPassword) || !filter_var($confirmPassword, FILTER_SANITIZE_SPECIAL_CHARS) || !preg_match('/^[A-Za-z][A-Za-z0-9]{0,254}$/', $confirmPassword)) {
+        $error[] = 'confirmPassword_incorrect';
+    }
+    if ($password != $confirmPassword) {
+        $error[] = 'samePassword_incorrect';
+    }
+
+    if (!empty($error)) {
+        return $error;
+    } else {
+        return false;
+    }
+}
+
+/**
+ * Function to check if user already exists in database
+ */
+function checkEmailInDataBase($conn, $email) {
+    $error = array();
+
+    $query = "SELECT * FROM user WHERE email = ?";
+
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, "s", $email);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_bind_result($stmt, $ID, $name, $email, $password, $dateOfBirth, $phoneNumber, $discord);
+    mysqli_stmt_store_result($stmt);
+
+    //Check if a result has been found with number of rows
+    if (mysqli_stmt_num_rows($stmt) > 0) {
+        mysqli_stmt_close($stmt);
+        mysqli_close($conn);
+        $error[] = 'email_in_use';
+        return $error;
+    } else {
+        mysqli_stmt_close($stmt);
+        return false;
+    }
+}
