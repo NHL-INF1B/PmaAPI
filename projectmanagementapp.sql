92a72d9d6ca9eb02bd566836e7ae50e257353f84
Chris <89904040+Clibzz@users.noreply.github.com>
2022-05-23 14:24:48 +0200

    Update projectmanagementapp.sql
    Updated the password character limit so the registration and login will actually be able to work properly & updated email character limit

--- projectmanagementapp.sql
+++ projectmanagementapp.sql
@@ -123,8 +123,8 @@ CREATE TABLE `timesheet` (
 CREATE TABLE `user` (
   `id` int(8) NOT NULL,
   `name` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `dateOfBirth` date NOT NULL,
   `phoneNumber` varchar(50) NOT NULL,
   `discord` varchar(50) NOT NULL
