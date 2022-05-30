db7268dd121a5cd506e277893b224a37adeb6e8f
Stefan <stefan.meijer3@student.nhlstenden.com>
2022-05-30 16:21:44 +0200

    warning

--- handlers/warning/warninghandler.php
+++ handlers/warning/warninghandler.php
@@ -2,13 +2,19 @@
 require_once('../../functions/database/dbconnect.php');
 require_once('../../functions/anti-cors/anticors.php');
 
+/**
+ * Getting posted data from the app
+ */
+$json = file_get_contents('php://input');
+$arr = json_decode($json, TRUE);
+
+if (isset($arr)) {
     $sql = "SELECT * FROM warning WHERE project_id = 1";
     $stmt = mysqli_prepare($conn, $sql);
     mysqli_stmt_execute($stmt);
     mysqli_stmt_store_result($stmt);
     mysqli_stmt_bind_result($stmt, $id, $reason, $user_id, $project_id);
 
-    echo "\n";
     $query = mysqli_query($conn, $sql);
     $result = array();
 
@@ -18,7 +24,6 @@ require_once('../../functions/anti-cors/anticors.php');
 
     //Send back response (JSON)
     echo json_encode($result);
-?>
-
-
-
+} else {
+    echo json_encode('No data send');
+}
