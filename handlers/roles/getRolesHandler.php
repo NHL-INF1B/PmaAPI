b205edd4f0f6cd143e039c1a050f15d2b409fed2
Marijke Baas <89772966+FreyaMarijke@users.noreply.github.com>
2022-05-30 19:20:00 +0200

    handlers voor gegevens halen uit en bijwerken van database tabel projectmember

---
+++ handlers/roles/getRolesHandler.php
@@ -0,0 +1,39 @@
+<?php
+require_once('../../functions/database/dbconnect.php');
+require_once('../../functions/anti-cors/anticors.php');
+
+/**
+ * Getting posted data from the app
+ */
+
+$json = file_get_contents('php://input');
+$arr = json_decode($json, TRUE); // returns array("username" => "stefan") etc.
+
+if (isset($arr)) {
+    $value = htmlentities($arr['value']);
+
+    if ($error = validateFields($value)) {
+        echo json_encode($error);
+    } else {
+        $userValues = array();
+
+        $queryuery = "SELECT * FROM `projectmember` WHERE project_id = ?";
+        $stmt = mysqli_prepare($conn, $query);
+        mysqli_stmt_bind_param($stmt, "i", $projectId);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $userId, $projectId, $roleId);
+
+        //All value's that will be send back to the application
+        $userValues[0]['user_id'] = $userId;
+        $userValues[0]['project_id'] = $projectId;
+        $userValues[0]['role_id'] = $roleId;
+        
+        mysqli_stmt_close($stmt);
+        mysqli_close($conn);
+
+        //Send back response (JSON)
+        echo json_encode($userValues);
+    }
+} else {
+    echo json_encode('No data send');
+}
