ffb9f8f7cc6e2c29b17047f7ec86a0705a2c49bd
Marijke Baas <89772966+FreyaMarijke@users.noreply.github.com>
2022-06-01 09:06:39 +0200

    aangepaste handlers voor ophalen en aanpassen van data uit de database

--- handlers/roles/getRolesHandler.php
+++ handlers/roles/getRolesHandler.php
@@ -16,13 +16,17 @@ if (isset($arr)) {
         echo json_encode($error);
     } else {
         $userValues = array();
+        $error = array();
 
+        //Requesting data from the database
         $queryuery = "SELECT * FROM `projectmember` WHERE project_id = ?";
         $stmt = mysqli_prepare($conn, $query);
         mysqli_stmt_bind_param($stmt, "i", $projectId);
         mysqli_stmt_execute($stmt);
         mysqli_stmt_bind_result($stmt, $userId, $projectId, $roleId);
+        while (mysqli_stmt_fetch($stmt)) {}
 
+        if (mysqli_stmt_num_rows($stmt) > 0) {
         //All value's that will be send back to the application
         $userValues[0]['user_id'] = $userId;
         $userValues[0]['project_id'] = $projectId;
@@ -33,7 +37,25 @@ if (isset($arr)) {
 
         //Send back response (JSON)
         echo json_encode($userValues);
+        } else {
+            $error[] = 'No_projectmembers';
+            echo json_encode($error);
+        }
     }
 } else {
     echo json_encode('No data send');
+}
+
+function validateFields($value) {
+    $error = array();
+
+    if (!isset($value)) {
+        $error[] = 'No_value_set';
+    }
+
+    if(!empty($error)) {
+        return $error;
+    } else {
+        return false;
+    }
 }
