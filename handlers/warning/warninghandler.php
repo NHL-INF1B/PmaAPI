f72e8798b561cab6cf2c6602bc6e17e01167153b
JesseOost <89914221+JesseOost@users.noreply.github.com>
2022-06-14 15:59:27 +0200

    no datafrom database handled

--- handlers/warning/warninghandler.php
+++ handlers/warning/warninghandler.php
@@ -23,7 +23,12 @@ if (isset($arr)) {
     }
 
     //Send back response (JSON)
-    echo json_encode($result);
+    if(!empty($result)){
+        echo json_encode($result);
+    } else{
+        echo json_encode("NO_DATA");
+    }
+
 } else {
     echo json_encode('No data send');
 }
