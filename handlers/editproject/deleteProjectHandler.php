0edf3404d0daea9bc36b4b1a67c6531e31360e90
Clibzz <chriscrossje2001@gmail.com>
2022-06-14 14:57:46 +0200

    Fix for deleting everything with a certain project_id when project is deleted

--- handlers/editproject/deleteProjectHandler.php
+++ handlers/editproject/deleteProjectHandler.php
@@ -11,7 +11,13 @@ $arr = json_decode($json, TRUE);
 if (isset($arr)) {
     $id = htmlentities($arr['id']);
 
-    $sql = "DELETE p, pm FROM project p JOIN projectmember pm ON pm.project_id = p.id WHERE p.id = ?";
+    $sql = "DELETE p, pm, w, i, s, t FROM project p 
+            JOIN projectmember pm ON pm.project_id = p.id 
+            JOIN warning w ON w.project_id = p.id
+            JOIN invite i ON i.project_id = p.id
+            JOIN schedule_line s ON s.project_id = p.id
+            JOIN timesheet t ON t.project_id = p.id
+            WHERE p.id = ?";
 
     //Sending data to the database
     $stmt = mysqli_prepare($conn, $sql);
