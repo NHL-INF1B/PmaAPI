4b3c73635632cc3500c27943ee77408a1a693795
JesseOost <89914221+JesseOost@users.noreply.github.com>
2022-06-16 15:56:07 +0200

    fixing planningOverzicht

--- handlers/planning/planningOverzicht.php
+++ handlers/planning/planningOverzicht.php
@@ -2,6 +2,8 @@
 require_once('../../functions/database/dbconnect.php');
 require_once('../../functions/anti-cors/anticors.php');
 
+// $projectid = 7;
+
 /**
  * Getting posted data from the app
  */
@@ -15,57 +17,24 @@ if (isset($arr)) {
     if ($error = false) { //Heeft nog validatie nodig! <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
         echo json_encode($error);
     } else {
-        $respond = array();
+        $results = array();
 
         //Sending data to the database
         $query = "SELECT id, week, activiteit FROM schedule_line WHERE project_id = ? ORDER BY week ASC";
         $stmt = mysqli_prepare($conn, $query);
         mysqli_stmt_bind_param($stmt, "i", $projectid);
         mysqli_stmt_execute($stmt);
-        mysqli_stmt_bind_result($stmt, $id, $week, $activiteit);
-        while (mysqli_stmt_fetch($stmt)) {
-            $multiArr = array();
-            $multiArr['title'] = 'Week '.$week;
-            $multiArr['data'] = [$activiteit];
-            $multiArr['id'] = $id;
-            array_push($respond, $multiArr);
-        }
 
-
+        $res = mysqli_stmt_get_result($stmt);
+        $result = mysqli_fetch_all($res, MYSQLI_ASSOC);
 
         //Close the statement and connection
         mysqli_stmt_close($stmt);
         mysqli_close($conn);
 
         //Send back response (JSON)
-        echo json_encode($respond);
+        echo json_encode($result);]
     }
 } else {
     echo json_encode('No data send');
 }
-
-// require_once('../../functions/database/dbconnect.php');
-// require_once('../../functions/anti-cors/anticors.php');
-
-//     $sql = "SELECT id, week, activiteit FROM schedule_line WHERE project_id = 1;";
-//     $stmt = mysqli_prepare($conn, $sql);
-//     mysqli_stmt_execute($stmt);
-//     mysqli_stmt_bind_result($stmt, $id, $week, $activiteit);
-//     mysqli_stmt_store_result($stmt);
-//     mysqli_stmt_fetch($stmt);
-
-//     $num = mysqli_stmt_num_rows($stmt);
-//     if($num > 0) {
-//         $results = array();
-
-//         do {
-//             $row = array($id, $week, $activiteit);
-//             array_push($results, $row);
-//         } while (mysqli_stmt_fetch($stmt));
-
-//         $json = "{ "arr": " . json_encode($results) . "}";
-
-//         echo json_encode($results);
-//         // var_dump(json_encode($results));
-//     }
-?>
