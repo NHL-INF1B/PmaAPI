6778e034f5bb818e5685dccb9f427775e5ea1cee
Clibzz <chriscrossje2001@gmail.com>
2022-06-20 14:42:22 +0200

    Scorebord fix

--- handlers/scorebord/scorebord.php
+++ handlers/scorebord/scorebord.php
@@ -11,14 +11,17 @@ $array = json_decode($json, TRUE);
 if (isset($array)) {
     $projectId = htmlentities($array['projectId']);
     
-    $sql = "SELECT user.id, user.name, projectmember.reward_points FROM user INNER JOIN projectmember ON user.id = projectmember.user_id WHERE project_id = ? ORDER BY projectmember.reward_points DESC";
+    $sql = "SELECT user.id, user.name, projectmember.reward_points 
+            FROM user 
+            INNER JOIN projectmember ON user.id = projectmember.user_id 
+            WHERE project_id = ? 
+            ORDER BY projectmember.reward_points DESC";
     $stmt = mysqli_prepare($conn, $sql);
     mysqli_stmt_bind_param($stmt, 'i', $projectId);
-    mysqli_execute($stmt);
+    mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
 
     $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
 
     $projectMembers = [];
 
@@ -29,5 +32,5 @@ if (isset($array)) {
     //Send back response (JSON)
     echo json_encode($projectMembers);
 } else {
-    echo json_encode('No data send');
+    echo json_encode('No data sent');
 }
