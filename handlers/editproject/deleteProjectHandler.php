b36b3fd29ca25a91b51a572c41e7b53a1aca03d5
FrancaBaars <franca.baars@student.nhlstenden.com>
2022-06-22 11:13:34 +0200

    Fixed delete project

--- handlers/editproject/deleteProjectHandler.php
+++ handlers/editproject/deleteProjectHandler.php
@@ -11,13 +11,14 @@ $arr = json_decode($json, TRUE);
 if (isset($arr)) {
     $id = htmlentities($arr['id']);
 
-    $sql = "DELETE p, pm, w, i, s, t FROM project p 
-            JOIN projectmember pm ON pm.project_id = p.id 
-            JOIN warning w ON w.project_id = p.id
-            JOIN invite i ON i.project_id = p.id
-            JOIN schedule_line s ON s.project_id = p.id
-            JOIN timesheet t ON t.project_id = p.id
-            WHERE p.id = ?";
+    $sql = "DELETE  p.*, pm.*, i.*, sl.*, ts.*, w.* 
+        FROM project AS p 
+        LEFT JOIN projectmember AS pm ON pm.project_id = p.id 
+        LEFT JOIN invite AS i ON i.project_id = p.id 
+        LEFT JOIN schedule_line AS sl ON sl.project_id = p.id 
+        LEFT JOIN timesheet AS ts ON ts.project_id = p.id 
+        LEFT JOIN warning AS w ON w.project_id = p.id 
+        WHERE p.id = ?";
 
     //Sending data to the database
     $stmt = mysqli_prepare($conn, $sql);
@@ -27,6 +28,7 @@ if (isset($arr)) {
     //Close the statement and connection
     mysqli_stmt_close($stmt);
     mysqli_close($conn);
+    echo json_encode("data_deleted");
 } else {
     echo json_encode('No data sent');
 }
