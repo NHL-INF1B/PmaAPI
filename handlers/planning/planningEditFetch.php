4a90ac581fb7eea70d4f9cdd3129cc87fbb5750b
Clibzz <chriscrossje2001@gmail.com>
2022-06-24 12:52:09 +0200

    PlanningEditFetch fix, connection doesnt close in while loop anymore

--- handlers/planning/planningEditFetch.php
+++ handlers/planning/planningEditFetch.php
@@ -6,48 +6,48 @@ require_once('../../functions/anti-cors/anticors.php');
  * Getting posted data from the app
  */
 $json = file_get_contents('php://input');
-$arr = json_decode($json, TRUE); // returns array("username" => "stefan") etc.
+$arr = json_decode($json, TRUE);
 
 if (isset($arr)) {
     $scheduleId = htmlentities($arr['scheduleId']);
 
     //Validate fields
-    if ($error = false) { //Heeft nog validatie nodig! <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    if ($error = false) {
         echo json_encode($error);
     } else {
         $error = array();
         $scheduleValues = array();
 
-        //Recieving data from the database
-        $query = "SELECT `id`, `week`, `activiteit` FROM schedule_line WHERE id = ?";
+        //Receiving data from the database
+        $query = "SELECT id, week, activiteit FROM schedule_line WHERE id = ?";
+
         $stmt = mysqli_prepare($conn, $query);
-        mysqli_stmt_bind_param($stmt, "s", $scheduleId);
+        mysqli_stmt_bind_param($stmt, "i", $scheduleId);
         mysqli_stmt_execute($stmt);
         mysqli_stmt_bind_result($stmt, $id, $week, $activity);
-        while (mysqli_stmt_fetch($stmt)) {}
+        mysqli_stmt_store_result($stmt);       
 
-        //Checking schedule line
+        while (mysqli_stmt_fetch($stmt)) { }
+
+        //Checking if there are any schedule_lines with the scheduleId
         if (mysqli_stmt_num_rows($stmt) > 0) {
             //All value's that will be send back to the application
-            $scheduleValues[0]['id'] = $id;
-            $scheduleValues[0]['week'] = $week;
-            $scheduleValues[0]['activity'] = $activity;
-
-            //Close the statement and connection
-            mysqli_stmt_close($stmt);
-            mysqli_close($conn);
-
-            //Send back response (JSON)
-            echo json_encode($scheduleValues);
+            $scheduleValues['id'] = $id;
+            $scheduleValues['week'] = $week;
+            $scheduleValues['activity'] = $activity;
+            $scheduleValues['projectId'] = $projectId;
         } else {
-            $error[] = 'Schedule line does not exists';
-            echo json_encode($error);
+            echo json_encode("No results");
         }
+
+        //Close the statement and connection
+        mysqli_stmt_close($stmt);
+        mysqli_close($conn);
+
+        //Send back response (JSON)
+        echo json_encode($scheduleValues);
     }
 } else {
-    echo json_encode('No data send');
+    echo json_encode('No data sent');
 }
 
-function checkIdInDB () {
-    
-}
